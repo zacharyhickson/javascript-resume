062d0ea94c40b5c091fd275a8f3f98d551438335
Zachary Hickson <zacharyh96@gmail.com>
2017-03-20 14:43:05 +1100

    practice with objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,36 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+//var awesomeThoughts = "I am Zachary and I am AWESOME!";
+
+//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+//$("#main").append(funThoughts);
+
+
+
+var skills = ["HTML", "CSS", "JavaScript", "WordPress"];
+
+var bio = {
+	"name" : "Zachary Hickson",
+	"role" : "Web Developer",
+	"contacts" : {
+		"mobile" : "0431198746",
+		"email" : "zacharyh96@gmail.com",
+		"location" : "Sydney, Australia"
+	},
+	"welcomeMessage" : "Welcome to my resume!",
+	"skills" : skills,
+	"bioPic" : "images/fry.jpg"
+};
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts[mobile]);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts[email]);
+
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
+$("#header").append(formattedMobile);
+$("#header").append(formattedEmail);
